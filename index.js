63f2bbe2fe2313c633217801ce8d57a1c2bc6541
GuriZenit <israelreis17@gmail.com>
2021-07-21 14:37:21 -0300

    update - index.js ready

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const Discord = require('discord.js')
 const config = require('./config.js')
 const client = new Discord.Client()
+
 client.login(config.token)
+client.on('ready', () => {console.log(`${client.user.tag} online`)})
