3aea3005cd3f5e26669d2a68fd5ebb7a622da74e
GuriZenit <israelreis17@gmail.com>
2021-07-23 18:05:57 -0300

    update - index.js added function send with embed support

--- index.js
+++ index.js
@@ -8,6 +8,23 @@ client.on('ready', () => {console.log(`${client.user.tag} online`)})
 
 client.ws.on('INTERACTION_CREATE', async inter => {
   
+  async function send(content, flag) {
+    const apiMessage = await Discord.APIMessage.create(client.channels.resolve(inter.channel_id), content)
+      .resolveData()
+      .resolveFiles();
+
+    client.api.interactions(inter.id,inter.token).callback.post({
+      data: {
+        type: 4,
+        data: {
+          ...apiMessage.data,
+          files: apiMessage.files,
+          flags: flag ? 1 << 6:undefined
+        }
+      }
+    })
+  }
+  
   const guild = client.guilds.cache.find(gu => gu.id == inter.guild_id)
   const command = inter.data.name.toLowerCase()
   const args = inter.data.options;
