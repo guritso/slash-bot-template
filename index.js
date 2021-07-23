4686dc083ea5088347c901dd22f8aa5c30bcc3d8
GuriZenit <israelreis17@gmail.com>
2021-07-23 17:57:38 -0300

    update - index.js interaction

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ const config = require('./config.js')
 const client = new Discord.Client()
 
 client.login(config.token)
+
 client.on('ready', () => {console.log(`${client.user.tag} online`)})
+
+client.ws.on('INTERACTION_CREATE', async inter => {
+})
