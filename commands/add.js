10bb62ef303f7e28454e99d362fed30c463281a2
GuriZenit <israelreis17@gmail.com>
2021-07-23 18:20:51 -0300

    update - add.js get command options and check json file

--- commands/add.js
+++ commands/add.js
@@ -1,4 +1,11 @@
 const json = require('../options/commands.json')
 module.exports.run = async(client, inter, args, guild, send) => {
+  if(!args) return;
   
+  const value = args[0].value.toUpperCase()
+  const name = args[0].name;
+  
+  if(!json[value]){
+    return send("🟥 | command not found", true)
+  }
 }
