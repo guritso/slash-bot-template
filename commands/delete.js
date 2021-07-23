1d2adeb3227105e1aa8c9d030d6e26f7db3212aa
GuriZenit <israelreis17@gmail.com>
2021-07-23 18:26:41 -0300

    update - remove.js function to get command id

--- commands/delete.js
+++ commands/delete.js
@@ -3,4 +3,14 @@ module.exports.run = async(client, inter, args, guild, send, msg) => {
   
   const value = args[0].value;
   const type = args[0].name;
+  
+  function getCommandId(arr, name){
+    for(i in arr){
+      if(arr[i].name === name){
+        send(`🟩 | command ${value} removed`, true)
+        return arr[i].id;
+      }
+    }
+    send("🟥 | command not found", true)
+  }
 }
