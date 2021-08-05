142c66fd4b21e76b93d5fd382b847db769b44899
GuriZenit <israelreis17@gmail.com>
2021-08-05 18:15:52 -0300

    update - avatar avatarURL()

--- commands/avatar.js
+++ commands/avatar.js
@@ -2,13 +2,16 @@ const Discord = require('discord.js')
 module.exports.run = async(client, inter, args, guild, send) => {
   
   const member = guild.members.cache.get(args ? args[0].value : inter.member.user.id)
- 
+  
+  const image = member.user.avatarURL({dinamic:true,format:'png',size:2048})
+  
   let embed = new Discord.MessageEmbed()
   .setTitle(member.user.tag)
   .setDescription('')
-  .setImage(member.user.avatarURL({size:2048}))
+  .setImage(image)
   .setFooter(`ID:${member.user.id}`)
   .setColor('#00FFAA')
-  
+ 
+  console.log(member.user.avatarURL({format:'png'}))
   send(embed)
 }
