cbc8968193d3f5243935ef02e592ac940474241b
israelreis17@gmail.com <israelreis17@gmail.com>
2022-11-14 15:19:02 -0300

    Update for the new version 14.x

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,13 +1,12 @@
-const { MessageEmbed } = require('discord.js')
+const { EmbedBuilder } = require('discord.js')
 module.exports.run = async(client, inter, guild) => {
   
   const member = guild.members.cache.get(inter.options.getUser('user') ? inter.options.get('user').value : inter.user.id)
   
   const image = member.user.avatarURL({dinamic:true,format:'png',size:4096})
   
-  let embed = new MessageEmbed()
+  let embed = new EmbedBuilder()
   .setTitle(member.user.tag)
-  .setDescription('')
   .setImage(image)
   .setFooter({
     text: `Requested by ${inter.user.tag}`,
