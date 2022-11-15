d91f530ec24688be9483ed6253e31c20e0851028
israelreis17@gmail.com <israelreis17@gmail.com>
2022-11-15 00:50:00 -0300

    changed the old delete method

--- commands/delete.js
+++ commands/delete.js
@@ -1,5 +1,8 @@
+const { REST, Routes } = require('discord.js')
+const { token } = require('../config.js')
 module.exports.run = async(client, inter, guild) => {
   
+  const rest = new REST({ version: '10'}).setToken(token)
   const type = inter.options._hoistedOptions[0].name;
   const value = inter.options.getString(type).toLowerCase()
   
@@ -7,13 +10,13 @@ module.exports.run = async(client, inter, guild) => {
     let gui = await client.api.applications(client.user.id).guilds(guild.id).commands.get()
     let id = getCommandId(gui, value)
     if(!id) return;
-    client.api.applications(client.user.id).guilds(guild.id).commands(id).delete()
+    rest.delete(Routes.applicationGuildCommand(client.user.id, guild.id, 'id'))
   }
   if(type == 'global'){
     let glo = await client.api.applications(client.user.id).commands.get()
     let id = getCommandId(glo, value)
     if(!id) return;
-    client.api.applications(client.user.id).commands(id).delete()
+    rest.delete(Routes.applicationCommand(client.user.id, 'id'))
   }
   
   function getCommandId(arr, name){
