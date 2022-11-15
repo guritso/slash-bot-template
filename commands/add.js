7ca6999369a4bfb5dce5ce9578ac469be402705b
israelreis17@gmail.com <israelreis17@gmail.com>
2022-11-14 21:22:59 -0300

    updated to new version of discord.js (14.x)

--- commands/add.js
+++ commands/add.js
@@ -1,27 +1,32 @@
 const json = require('../options/commands.json')
+const { REST, Routes } = require ('discord.js')
+const { token }= require('../config.js')
 module.exports.run = async(client, inter, guild) => {
+  
+  const rest = new REST({ version: '10'}).setToken(token)
   
   const type = inter.options._hoistedOptions[0].name;
   
   const value = inter.options.getString(type).toUpperCase()
   
   if(!json[value]){
-    return inter.reply({
+    return await inter.reply({
       content: "✖️ | command not found",
       ephemeral: true
     })
   }
   
   if(type == 'guild'){
-    client.api.applications(client.user.id)
-    .guilds(guild.id)
-    .commands.post({data: json[value]})
+    await rest.put(
+     Routes.applicationGuildCommands(client.user.id, guild.id),
+    { body: json[value] })
   }
   if(type == 'global'){
-    client.api.applications(client.user.id)
-    .commands.post({data: json[value]})
+    await rest.put(
+      Routes.applicationCommands(client.user.id),
+      {body: json[value]})
   }
-  inter.reply({
+  await inter.reply({
     content: `➕ | command ${value} added`,
     ephemeral: true
   })
