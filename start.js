282529f7345066c57a74208c54188276587457db
israelreis17@gmail.com <israelreis17@gmail.com>
2022-11-17 21:47:10 -0300

    #1 syntax correction (start.js)

--- start.js
+++ start.js
@@ -2,16 +2,20 @@
 // you only need to use this file once
 // add your bot and guild id here
 // then use `node start.js` on a terminal
-/*/ or add `require(./start.js)` on a ready envet in the index.js in case you're not in a terminal /*/
-const { REST, Routes } = require('discord.js')
-const { token } = require('./config.js')
-const json = require('./slash/commands.json')
-const guildID = 'GUILD_ID_HERE';
-const clientID = 'BOT_ID_HERE';
-const rest = new REST({ version: '10' }).setToken(token);
+/*/ or add `require(./start.js)` on a ready envet in the index.js in case you"re not in a terminal /*/
+const { REST, Routes } = require("discord.js");
+const { token } = require("./config.js");
+const json = require("./slash/commands.json");
+const guildID = "GUILD_ID_HERE";
+const clientID = "BOT_ID_HERE";
+const rest = new REST({ version: "10" }).setToken(token);
 
 (async () => {
-  console.log('adding first command...')
-  await rest.post(Routes.applicationGuildCommands(clientID, guildID), { body: json['ADD'] }).then(
-    console.log('success!'))
-})();
+  console.log("adding first command...");
+  await rest
+    .post(Routes.applicationGuildCommands(clientID, guildID), {
+      body: json["ADD"],
+    })
+    .then(console.log("success!"));
+  })
+();
