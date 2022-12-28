7ff524fadfb488dc61a6802d2dad16279022fad2
gurizenit <gurizenit@gmail.com>
2022-12-28 13:29:33 -0300

    update: to rebuild in add.js

--- src/Commands/Developer/add.js
+++ src/Commands/Developer/add.js
@@ -1,6 +1,5 @@
 const { Routes, REST } = require("discord.js");
 const config = require("../../Configs/config.js");
-const { readFileSync } = require("node:fs");
 const rest = new REST({ version: "10" }).setToken(config.token);
 
 module.exports = {
@@ -36,35 +35,34 @@ module.exports = {
   },
   async execute(interaction, client) {
     const guild = interaction.guild;
-    // read the commands.json
-    const PATH = "slash/commands.json";
-    const jsonFile = JSON.parse(readFileSync(PATH));
     // get the input (command name)
     const name = interaction.options.getString("name").toUpperCase();
     // get the type of the command guild/global
     const type = interaction.options.getString("type").toUpperCase();
-    // check the command in the json file
-    if (!jsonFile[name]) {
+    // get the command
+    const command = client.commands.get(name);
+    console.log(command)
+    if (!command) {
       return interaction.reply({
         embeds: [
           {
-            title: `${name} ⊗ NOT FOUND in JSON`,
-            color: 0xd8303b, // = #D8303B
+            title: `${name} ⊗ NOT FOUND in the FILE`,
+            color: 0xd8303b, // = #D8303B = red
           },
         ],
         ephemeral: true,
       });
     }
     // register the command globally, else in a guild
-    if (type == "GLOBAL") {
+    if (type === "GLOBAL") {
       await rest.post(Routes.applicationCommands(client.user.id), {
-        body: jsonFile[name],
+        body: command.data,
       });
     } else {
       await rest.post(
         Routes.applicationGuildCommands(client.user.id, guild.id),
         {
-          body: jsonFile[name],
+          body: command.data,
         }
       );
     }
@@ -72,7 +70,7 @@ module.exports = {
       embeds: [
         {
           title: `${name} ⊕ ADDED to ${type}`,
-          color: 0x00ffaa, // = #00FFAA
+          color: 0x00ffaa, // = #00FFAA = green
         },
       ],
       ephemeral: true,
